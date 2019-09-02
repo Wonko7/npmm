e236e33b7b8fd528cba1b7c4f1a71ca988e75fd4
William <william@undefined.re>
2019-09-02 23:49:44 +0200

    readable map format

--- gestion_map.ml
+++ gestion_map.ml
@@ -17,15 +17,26 @@ end;;
 
 
 
-let map=open_in "map.txt";;
+let map=open_in "map2.txt";;
 let abscisse = int_of_string (input_line map);;
 let ordonnee = int_of_string (input_line map);;
 
 let mappy = let tmp = new case 0 in create_matrix ordonnee  abscisse tmp;;
 
 
+
+let int_of_explicit_char c = match c with
+    '^' -> 2
+  | 'v' -> 5
+  | '>' -> 3
+  | '<' -> 7
+  | '#' -> 0
+  | '\n' -> 33       (* new line... *)
+  | c -> int_of_char c
+
+
 let rec store m map x y =
-        try let n = int_of_char(input_char map) in
+        try let n = int_of_explicit_char (input_char map) in
         if (n<>33) then (if (y < ordonnee) && (x < abscisse) then (let tmp = new case n in m.(y).(x)<- tmp) else ();
                          store m map (x+1) y)
                    else store m map 0 (y+1);
